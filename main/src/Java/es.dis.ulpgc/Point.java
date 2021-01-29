c0907bf2da2d179350edc15ea2fff1d8efb40ef0
Romel <romel19995@gmail.com>
2021-01-29 11:37:52 +0000

    cambios en Point

--- main/src/Java/es.dis.ulpgc/Point.java
+++ main/src/Java/es.dis.ulpgc/Point.java
@@ -1,12 +1,12 @@
 package es.dis.ulpgc;
 
 public class Point {
-    private String [] point;
+    private String point;
 
-    public Point(String [] point) {
-        this.point = new String [point];
+    public Point(String point) {
+        this.point = point;
     }
 
-    public int getPoints() { return point; }
+    public int getPoint() { return point; }
 
 }
